919a611e98f3e19d1ad764cdccb8d1b78a0cb6b3
Y Jian <32028985+jianyiren0621@users.noreply.github.com>
2017-10-23 03:20:54 -0700

    Add files via upload

---
+++ write_DF_CDKs.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+import pandas as pd
+
+rootdir = './CDK_datasets'
+
+#The below is a tedious, but easy understanding of using os to get all the rootdir ecfp files
+subfolder = []
+for root, dirs, files in os.walk(rootdir):
+    a = []
+    for name in files:
+        if name.startswith("id_df"):
+            a.append(os.path.join(root, name))
+    subfolder.append(a)
+
+sub =  subfolder[1:]# The fisrt element of subfolder is [], we need to drop it.
+print sub
+
+# sub = [['./CDK_datasets/CHEMBL1907602-P06493/id_df_bit_CHEMBL1907602-P06493_info.csv'], ['./CDK_datasets/CHEMBL2094128-P24941/id_df_bit_CHEMBL2094128-P24941_info.csv']]
+
+def read1folder(a):
+    df = pd.DataFrame.from_csv(a[0])
+    CHEMBL_id = a[0].split('/')[2].split('-')[0]
+    df2 = df.loc[:,['compound','new_value']]
+    df2.set_index('compound',inplace = True)
+    df2.rename(columns={'new_value': CHEMBL_id,}, inplace = True)
+    return list(df['compound']), df2
+
+drug_pool = []
+DF = pd.DataFrame()
+total_count = 0
+for a in sub:
+    tmp = pd.DataFrame()
+    sub_pool, tmp = read1folder(a)
+    DF = pd.concat([DF, tmp], axis=1)
+    total_count = total_count + len(sub_pool)
+    print len(sub_pool)
+    for id in sub_pool:
+        if id not in drug_pool:
+            drug_pool.append(id)
+
+print len(drug_pool), total_count
+DF.to_csv('DF_for_CDKs.csv')
+
