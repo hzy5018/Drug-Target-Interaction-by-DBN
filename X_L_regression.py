7763c0a1ecb326df4993e2b4a2166a11627104db
Y Jian <32028985+jianyiren0621@users.noreply.github.com>
2017-10-27 15:34:17 -0700

    Update X_L_regression.py

--- X_L_regression.py
+++ X_L_regression.py
@@ -2,25 +2,33 @@ import os
 import numpy as np
 import pandas as pd
 
-drugfile = "CDK_datasets/CHEMBL301-P24941/ecfp_bit_CHEMBL301-P24941_info.csv"
+rootdir = './CDK_datasets'
 
-target = []
-canonical_smiles = []
-ecfp = []
-measurement_value = []
-new_value = []
-measurement_type = []
-with open(drugfile) as f:
-    # skip the first line of file
-    next(f)
-    for line in f:
-        each_line = line.split('" "') # Seperate each column by " ".
-        target.append( each_line[0].replace('"','') )
-        canonical_smiles.append( each_line[1] )
-        ecfp.append( each_line[2] )
-        measurement_value.append( float(each_line[3]) )
-        new_value.append( float(each_line[4]) )
-        measurement_type.append( each_line[5] )
+f_test =  [os.path.join(root, name)
+           for root, dirs, files in os.walk(rootdir)
+           for name in files
+           if name.startswith("ecfp")]
+
+print f_test
+
+def read1folder(a):
+    target = []
+    canonical_smiles = []
+    ecfp = []
+    measurement_value = []
+    new_value = []
+    measurement_type = []
+    with open(a) as f:
+        # Skip the first line of file
+        next(f)
+        for line in f:
+            each_line = line.split('" "') # Seperate each column by " ".
+            target.append( each_line[0].replace('"','') )
+            canonical_smiles.append( each_line[1] )
+            ecfp.append( each_line[2] )
+            measurement_value.append( float(each_line[3]) )
+            new_value.append( float(each_line[4]) )
+            measurement_type.append( each_line[5] )
 
     df = pd.DataFrame()
     df['target'] = pd.Series(target)
@@ -30,7 +38,7 @@ with open(drugfile) as f:
     df['new_value'] = pd.Series(new_value)
     df['measurement_type'] = pd.Series(measurement_type)
     # df.to_csv('df.csv')
-
+    
     df_sort = df.sort_values('new_value')
     # df_sort.to_csv('df_sort.csv')
 
@@ -45,8 +53,12 @@ with open(drugfile) as f:
             ecfp.append(int(n))
         b.append(ecfp)
 
-print len(b)
+    path =  a.split('ecfp')[0]
+    name =  a.split('ecfp')[1].split('_info')[0]
+    np.savetxt(path + 'X' + name + '.txt', b, fmt = '%d')
+    np.savetxt(path + 'Y' + name + '.txt', log_value, fmt = '%d')
+    print len(b), len(log_value)
 
-np.savetxt('Model/regression/X.txt', b, fmt = '%d')
-np.savetxt('Model/regression/Y.txt', log_value, fmt = '%.3f')
+for a in f_test:
+    read1folder(a)
 
